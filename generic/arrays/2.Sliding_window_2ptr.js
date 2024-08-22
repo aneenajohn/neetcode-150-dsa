307ef85f5a09ba91014e81a9a9ab5cca1ffd896d
aneena <aneenajohn@gmail.com>
2024-08-23 00:39:36 +0530

    Max lebgth of sub array with sum <= k case with Optimised approach added
    Signed-off-by: aneena <aneenajohn@gmail.com>

--- generic/arrays/2.Sliding_window_2ptr.js
+++ generic/arrays/2.Sliding_window_2ptr.js
@@ -4,26 +4,57 @@
 const arr = [2, 5, 1, 7, 10];
 const k = 14;
 
-let maxSum = 0,
-  maxLength = 0;
-
-// Brute force:
-// Meth: Generate sum of all sub arrays and find their sum
-for (let i = 0; i < arr.length; i++) {
-  let sum = 0;
-  for (j = i; j < arr.length; j++) {
-    sum += arr[j];
-    console.log(arr[i], arr[j], sum);
-
-    if (sum <= k) {
-      let currentLength = j - i + 1;
-      if (currentLength > maxLength) {
-        maxSum = sum;
-        console.log('length: ', i, j, j - i + 1);
-        maxLength = currentLength;
-      }
+// NOTE:: Brut force:
+function maxLengthOfSubArr(arr, k) {
+  let n = arr.length,
+    maxSum = 0;
+  let maxLength = 0;
+  // Meth: Generate sum of all sub arrays and find their sum
+  for (let i = 0; i < n; i++) {
+    let sum = 0;
+    for (let j = i; j < n; j++) {
+      sum += arr[j];
+      if (sum <= k) {
+        let currLen = j - i + 1;
+        if (currLen > maxLength) {
+          maxSum = Math.max(sum, maxSum);
+          maxLength = currLen;
+        }
+      } else if (sum > k) break;
     }
   }
+  return { maxSum, maxLength };
 }
 
-console.log('Max length of subarray: ', maxLength);
+console.log('Max length of subarray: ', maxLengthOfSubArr(arr, k));
+
+// NOTE: Better: Sliding Window + 2 pointer
+
+// const arr = [2, 5, 1, 7, 10];
+function maxLengthOfSubArrWithSW(arr, k) {
+  let left = 0,
+    right = 0,
+    sum = 0,
+    maxSum = 0,
+    maxLength = 0;
+
+  while (right < arr.length) {
+    sum += arr[right];
+
+    while (sum > k && left <= right) {
+      sum -= arr[left];
+      left++;
+    }
+
+    if (sum < k) {
+      maxSum = Math.max(sum, maxSum);
+      maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    right++;
+  }
+
+  return { maxSum, maxLength };
+}
+
+console.log('Better Max length of subarray: ', maxLengthOfSubArrWithSW(arr, k));
