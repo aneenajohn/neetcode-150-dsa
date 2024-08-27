522b10c3b9b32417dc74559bd5b7793efcb9dd2f
aneena <aneenajohn@gmail.com>
2024-08-27 23:56:44 +0530

    Add time and space complexity
    Signed-off-by: aneena <aneenajohn@gmail.com>

--- striver/arrays-sliders-&-2-pointers/3.max_point_from_n_cards.js
+++ striver/arrays-sliders-&-2-pointers/3.max_point_from_n_cards.js
@@ -8,17 +8,21 @@ function maxPoints(arr, k) {
   let sum = 0,
     maxSum = 0;
   for (let i = 0; i < k; i++) {
+    // DOCS: TC -- O(k)
     sum += arr[i];
   }
   maxSum = sum;
 
   let right = arr.length;
   for (let left = k - 1; left >= 0; left--) {
+    // DOCS: TC -- O(k)
     right--;
     console.log('left: ', left, arr[left], 'right: ', right, arr[right]);
     sum = sum - arr[left] + arr[right];
     maxSum = Math.max(sum, maxSum);
   }
+
+  // DOCS: TC: O(K) +O(K) = O(2K) And SC: O(1) because the function only uses a constant amount of extra space (for variables like sum, maxSum, left, right).
 
   return maxSum;
 }
