b4be8c262e7368b1c0c12325c9a052c3fd10740c
aneena <aneenajohn@gmail.com>
2024-09-13 23:41:59 +0530

    Update docs on optimised soln and minor code refactor
    Signed-off-by: aneena <aneenajohn@gmail.com>

--- striver/sliders-&-2-pointers/8.num_of_subStrs_with_all3_chars.js
+++ striver/sliders-&-2-pointers/8.num_of_subStrs_with_all3_chars.js
@@ -47,24 +47,24 @@ function countOfSubStrWithAll3Chars_v2(str) {
 // console.log(countOfSubStrWithAll3Chars_v2(s));
 
 // DOCS: With every char thr is a subStr that ends:
-
+// if bbacba is a word given, and you found that b(bac)ba here the bac the first substring containing all 3 chars ending with the char c, so all the sub strings before that can be counted as valid one
+//b(bac)ba => 2 substr bac,bbac
+//bb(acb)a => 3 substr acb,bacb, bbacb
+//bba(cba) => 4 substr cba, acba, bacba, bbacba
+// Optimised = O(n)
 function countOfSubStrWithAll3Chars_v3(str) {
   const strMap = new Map();
   let count = 0;
   for (let i = 0; i < str.length; i++) {
-    strMap.set(str[i], i);
+    if (str[i] === 'a' || str[i] === 'b' || str[i] === 'c') {
+      strMap.set(str[i], i);
+    }
     if (strMap.size === 3) {
-      let endIndex = Math.max(
-        strMap.get('a'),
-        strMap.get('b'),
-        strMap.get('c')
-      );
       let startIndex = Math.min(
         strMap.get('a'),
         strMap.get('b'),
         strMap.get('c')
       );
-
       count += startIndex + 1;
     }
   }
