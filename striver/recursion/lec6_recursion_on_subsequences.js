5bca019691a10e54c5aa8bedd63cd0a4bdf46846
aneena <aneenajohn@gmail.com>
2024-10-06 12:22:21 +0530

    Add space and time complexity for print all sub seqs
    Signed-off-by: aneena <aneenajohn@gmail.com>

--- striver/recursion/lec6_recursion_on_subsequences.js
+++ striver/recursion/lec6_recursion_on_subsequences.js
@@ -1,4 +1,5 @@
-// DOCS: Print all subsequences -- A contiguous/non-contiguous sequences which follows the order
+// ?Print all subsequences of the given for eg: arr = [3,1,2] => 
+// DOCS: 1. Print all subsequences -- A contiguous/non-contiguous sequences which follows the order
 // At each index, you have 2 options, whether to take an element or skip(no take)
 const arr = [3, 1, 2];
 function printAllSubseq(ind, arr, originalArr) {
@@ -6,12 +7,28 @@ function printAllSubseq(ind, arr, originalArr) {
     console.log(arr);
     return;
   }
-  // Take(Include) the current element
+  // Take(Include) the current element in your sub seq
   arr.push(originalArr[ind]);
   printAllSubseq(ind + 1, arr, originalArr);
-  // No Take(Exclude) the current element
+  // No Take(Exclude) the current element, this element wont be added to your sub seq
   arr.pop();
   printAllSubseq(ind + 1, arr, originalArr);
 }
 
 printAllSubseq(0, [], arr);
+
+// DOCS:Time Complexity Analysis:
+
+// For each element in the array, we have 2 choices (include or exclude)
+// The array has n elements
+// Therefore, for each subsequence, we make n decisions, and each decision has 2 possibilities
+// This leads to 2^n possible subsequences _2_ _2_ _2_ => 2^n
+
+// So, the time complexity is O(2^n).
+
+// Space for storing subsequences:
+
+// The arr parameter in the function stores the current subsequence
+// The maximum size of this array would be n
+// Therefore, O(n) space is used for storing each subsequence
+
